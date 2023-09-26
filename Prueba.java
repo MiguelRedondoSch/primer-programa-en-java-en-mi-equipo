f61ebcc201ba92254b713fa1f2bf0d195f2bd41d
MIGUEL REDONDO GARCIA <miguel.redgar@educa.jcyl.es>
2023-09-26 12:30:40 +0200

    Edad y nacimiento

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
     public static void main(String args [])
     {
         System.out.println("Redondo García");
+        System.out.println("Tengo 17 años, nací en 2005");
     }
 }
